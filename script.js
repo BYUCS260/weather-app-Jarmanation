e03f88276be05516642e41cd8153fad38c25bd02
Jarmanation <46572667+Jarmanation@users.noreply.github.com>
2021-11-16 16:33:11 -0700

    all
    done

--- script.js
+++ script.js
@@ -10,11 +10,15 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
       return response.json();
     }).then(function(json) {
       let results = "";
+      console.log(json);
       results += '<h2>Weather in ' + json.name + "</h2>";
       for (let i=0; i < json.weather.length; i++) {
 	results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
       }
+      results += "<p>" + "Current Temp" + "</p>"
       results += '<h2>' + json.main.temp + " &deg;F</h2>"
+      results += "<p>" + "Feels Like" + "</p>"
+      results += '<h2>' + json.main.feels_like + " &deg;F</h2>"
       results += "<p>"
       for (let i=0; i < json.weather.length; i++) {
 	results += json.weather[i].description
@@ -23,17 +27,18 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
       }
       results += "</p>";
       document.getElementById("weatherResults").innerHTML = results;
-
     });
-const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=APIKEY";
+const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ",US&units=imperial" + "&APPID=d45b38e589d38f752354be62f37bcf98";
   fetch(url2)
     .then(function(response) {
       return response.json();
     }).then(function(json) {
       let forecast = "";
+      console.log(json);
       for (let i=0; i < json.list.length; i++) {
 	forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
-	forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
+	forecast += "<p>Temperature: " + json.list[i].main.temp + " &deg;F</p>";
+  forecast += "<p>Humidity: " + json.list[i].main.humidity + '%'+"</p>";
 	forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
       }
       document.getElementById("forecastResults").innerHTML = forecast;
